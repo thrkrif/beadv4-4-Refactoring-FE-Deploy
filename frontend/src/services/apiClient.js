4608b7dbc4e551aa1cb9f8ce87a52a17271836f7
thrkrif <103479629+thrkrif@users.noreply.github.com>
2026-01-25 15:24:12 +0900

    chore: api base url 수정

--- frontend/src/services/apiClient.js
+++ frontend/src/services/apiClient.js
@@ -1,5 +1,5 @@
 // const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://15.164.161.36';
-const API_BASE_URL = 'https://15.164.161.36';
+const API_BASE_URL = 'https://api.thock.site';
 
 class ApiClient {
     constructor() {
